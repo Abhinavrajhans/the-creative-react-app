d28335d8391ef6f7a177266f99b662de86f1a1d4
ABHINAV RAJHANS <68992789+Abhinavrajhans@users.noreply.github.com>
2021-03-26 16:57:41 +0530

    initialize

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AboutSection from "./components/AboutSection";
+function App() {
+  return (
+    <div className="App">
+      <h1>Film</h1>
+      <AboutSection />
+    </div>
+  );
+}
+
+export default App;
